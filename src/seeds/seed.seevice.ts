09e5af7392d8b37752531121d64cd6a88c16c7e4
hanafi <hanafiadhi890@gmail.com>
2024-07-05 15:03:30 +0700

    change data seed
    Signed-off-by: hanafi <hanafiadhi890@gmail.com>

--- src/seeds/seed.seevice.ts
+++ src/seeds/seed.seevice.ts
@@ -19,7 +19,7 @@ export class SeedService {
         password: 'root',
         role: ['root', 'admin'],
         applications: ['webiste', 'dashboard', 'mobile'],
-        tenant_id: 'PDP0001',
+        tenant_id: 'pdp00001',
       };
       user.password = await this.hashingService.hash(user.password);
       const findUser = await this.userModel.findOne({
